789ceb3c82adfd2109ede21d219125aa102bc366
bala-amypo <bala@amypo.in>
2025-12-16 17:29:10 +0000

    User commit

--- src/main/java/com/example/demo/model/MicroLesson.java
+++ src/main/java/com/example/demo/model/MicroLesson.java
@@ -30,30 +30,74 @@ public class MicroLesson {
     }
     public long getId()
     {
-        return
+        return id;
     }
 
     public void setCourse(Course course)
     {
-        
+        this.course=course;
     }
     public Course getCourse()
+    {
+        return course;
+    }
 
     public void setTitle(String title)
+    {
+        this.title=title;
+
+    }
     public String getTitle()
+    {
+        return title;
+    }
 
     public void setDurationMinutes(Integer durationMinutes)
+    {
+        this.durationMinutes=durationMinutes;
+
+    }
     public Integer getDurationMinutes()
+    {
+        return durationMinutes;
+    }
 
     public void setContentType(String contentType)
+    {
+        this.contentType=contentType;
+
+    }
     public String getContentType()
+    {
+        return contentType;
+    }
 
     public void setDifficulty(String difficulty)
+    {
+        this.difficulty=difficulty;
+
+    }
     public String getDifficulty()
+    {
+        return difficulty;
+    }
 
     public void setTags(String tags)
+    {
+        this.tags=tags;
+
+    }
     public String getTags()
+    {
+        return tags;
+    }
 
     public void setPublishDate(LocalDate publishDate)
+    {
+        this.publishDate=publishDate;
+    }
     public LocalDate getPublishDate()
+    {
+        return publishDate;
+    }
 }
--- src/main/java/com/example/demo/model/Progress.java
+++ src/main/java/com/example/demo/model/Progress.java
@@ -10,4 +10,6 @@ public class Progress {
     private Double score;
     
     public Progress(){}
+
+    
 }
