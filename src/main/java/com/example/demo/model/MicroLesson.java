97c43c3b938bd813b1bd992b108e345834de9f95
bala-amypo <bala@amypo.in>
2025-12-16 17:19:10 +0000

    User commit

--- src/main/java/com/example/demo/model/MicroLesson.java
+++ src/main/java/com/example/demo/model/MicroLesson.java
@@ -16,6 +16,35 @@ public class MicroLesson {
     public MicroLesson(Course course,String title,Integer durationMinutes,String contentType,String difficulty,String tags,LocalDate publishDate)
     {
         this.course=course;
-        tg
+        this.title=title;
+        this.durationMinutes=durationMinutes;
+        this.contentType=contentType;
+        this.difficulty=difficulty;
+        this.tags=tags;
+        this.publishDate=publishDate;
     }
+
+    public void setId()
+    public long getId()
+
+    public void setCourse()
+    public getCourse()
+
+    public void setTitle()
+    public getTitle
+
+    public void setDurationMinutes()
+    public getDurationMinutes()
+
+    public void setC
+    public
+
+    public void
+    public
+
+    public void
+    public
+
+    public void
+    public
 }
