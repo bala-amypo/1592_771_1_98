c9a9db6157458ad5c14967e32c30b553636ccef1
bala-amypo <bala@amypo.in>
2025-12-16 17:44:10 +0000

    User commit

--- src/main/java/com/example/demo/model/Recommendation.java
+++ src/main/java/com/example/demo/model/Recommendation.java
@@ -9,4 +9,37 @@ public class Recommendation {
     private Double confidenceScore;
 
     public Recommendation(){}
+
+    public Recommendation(User user,LocalDateTime generatedAt,String recommendedLessonIds,String basisSnapshot,Double confidenceScore)
+    {
+        this.user=user;
+        this.generatedAt=generatedAt;
+        this.recommendedLessonIds=recommendedLessonIds;
+        this.basisSnapshot=basisSnapshot;
+        this.confidenceScore=confidenceScore;
+    }
+
+    public void setId()
+    {
+        this.id=id;
+    }
+    public long getId()
+
+    public void setUser()
+    {
+        
+    }
+    public User getUser();
+
+    public void setGeneratedAt();
+    public LocalDateTime getGeneratedAt()
+
+    public void setRecommendedLessonIds()
+    public String getRecommendedLessonIds()
+
+    public void setBasisSnapshot()
+    public String getBasisSnapshot()
+
+    public void setConfidenceScore()
+    public Double getConfidenceScore()
 }
