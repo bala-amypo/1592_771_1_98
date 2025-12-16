3fa113a3131d739371f379c7d08f9689bfc4bda7
bala-amypo <bala@amypo.in>
2025-12-16 17:09:10 +0000

    User commit

--- src/main/java/com/example/demo/model/MicroLesson.java
+++ src/main/java/com/example/demo/model/MicroLesson.java
@@ -2,4 +2,16 @@ package com.example.demo.model;
 
 public class MicroLesson {
     
+    private long id;
+    private Course course;
+    private String title;
+    private Integer durationMinutes;
+    private String contentType;
+    private String difficulty;
+    private String tags;
+    private LocalDate publishDate;
+
+    public MicroLesson(){}
+
+    public 
 }
