69ea1d91560abd4ded80b130857c66331b59ee81
bala-amypo <bala@amypo.in>
2025-12-17 04:30:35 +0000

    User commit

--- src/main/java/com/example/demo/model/Progress.java
+++ src/main/java/com/example/demo/model/Progress.java
@@ -1,86 +1,102 @@
 package com.example.demo.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+@Entity
 public class Progress {
-    private long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private User user;
+
     private MicroLesson microLesson;
+
     private String status;
+
+    @Min(value = 1)
+    @Max(value = 99)
     private Integer progressPercent;
+
     private LocalDateTime lastAccessedAt;
+
+    @Column(nullable = true)
     private Double score;
-    
+
+    public Progress(User user, MicroLesson microLesson, String status, Integer progressPercent,
+            LocalDateTime lastAccessedAt, Double score) {
+        this.user = user;
+        this.microLesson = microLesson;
+        this.status = status;
+        this.progressPercent = progressPercent;
+        this.lastAccessedAt = lastAccessedAt;
+        this.score = score;
+    }
+
     public Progress(){}
 
-    public MicroLesson(User user,MicroLesson microLesson,String status,Integer progressPercent,LocalDateTime lastAccessedAt,Double score)
-    {
-        this.user=user;
-        this.microLesson=microLesson;
-        this.status=status;
-        this.progressPercent=progressPercent;
-        this.lastAccessedAt=lastAccessedAt;
-        this.score=score;
-    }
-
-    public void setId(long id)
-    {
-        this.id=id;
-    }
-    public long getId()
-    {
+    public Long getId() {
         return id;
     }
 
-    public void setUser(User user)
-    {
-        this.user=user;
-    }
-    public User getUser()
-    {
-        return id;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public void setMicroLesson(MicroLesson microLesson)
-    {
-        this.microLesson=microLesson;
+    public User getUser() {
+        return user;
     }
-    public MicroLesson getMicroLesson()
-    {
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public MicroLesson getMicroLesson() {
         return microLesson;
     }
 
-    public void setStatus(String status)
-    {
-        this.status=status;
+    public void setMicroLesson(MicroLesson microLesson) {
+        this.microLesson = microLesson;
     }
-    public String getStatus()
-    {
+
+    public String getStatus() {
         return status;
     }
 
-    public void setProgressPercent(Integer progressPercent)
-    {
-        this.progressPercent=progressPercent;
+    public void setStatus(String status) {
+        this.status = status;
     }
-    public Integer getProgressPercent()
-    {
+
+    public Integer getProgressPercent() {
         return progressPercent;
     }
 
-    public void setLastAccessedAt(LocalDateTime lastAccessedAt)
-    {
-        this.lastAccessedAt=lastAccessedAt;
+    public void setProgressPercent(Integer progressPercent) {
+        this.progressPercent = progressPercent;
     }
-    public LocalDateTime getLastAccessedAt()
-    {
+
+    public LocalDateTime getLastAccessedAt() {
         return lastAccessedAt;
     }
 
-    public void setScore(Double score)
-    {
-        this.score=score;
+    public void setLastAccessedAt(LocalDateTime lastAccessedAt) {
+        this.lastAccessedAt = lastAccessedAt;
     }
-    public Double getScore()
-    {
+
+    public Double getScore() {
         return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 }
