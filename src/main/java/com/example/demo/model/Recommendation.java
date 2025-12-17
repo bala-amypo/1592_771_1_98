c4c7df93f6d2502be3dc03585f00d8b2a81f0914
bala-amypo <bala@amypo.in>
2025-12-17 04:50:59 +0000

    User commit

--- src/main/java/com/example/demo/model/Recommendation.java
+++ src/main/java/com/example/demo/model/Recommendation.java
@@ -1,78 +1,92 @@
 package com.example.demo.model;
 
-public class Recommendation {
-    private long id;
-    private User user;
-    private LocalDateTime generatedAt;
-    private String recommendedLessonIds;
-    private String basisSnapshot;
-    private Double confidenceScore;
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+@Entity
+public class Recommendation {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private User user;
+
+    private LocalDateTime generatedAt;
+
+    private String recommendedLessonIds;
+
+    private String basisSnapshot;
+
+    @Min(value = 0)
+    @Max(value = 1)
+    private Double confidenceScore;
+    
     public Recommendation(){}
 
-    public Recommendation(User user,LocalDateTime generatedAt,String recommendedLessonIds,String basisSnapshot,Double confidenceScore)
-    {
-        this.user=user;
-        this.generatedAt=generatedAt;
-        this.recommendedLessonIds=recommendedLessonIds;
-        this.basisSnapshot=basisSnapshot;
-        this.confidenceScore=confidenceScore;
+    public Recommendation(User user, LocalDateTime generatedAt, String recommendedLessonIds, String basisSnapshot,
+            Double confidenceScore) {
+        this.user = user;
+        this.generatedAt = generatedAt;
+        this.recommendedLessonIds = recommendedLessonIds;
+        this.basisSnapshot = basisSnapshot;
+        this.confidenceScore = confidenceScore;
     }
 
-    public void setId(long id)
-    {
-        this.id=id;
-    }
-    public long getId()
-    {
+    public Long getId() {
         return id;
     }
 
-    public void setUser(User user)
-    {
-        this.user=user;
+    public void setId(Long id) {
+        this.id = id;
     }
-    public User getUser()
-    {
+
+    public User getUser() {
         return user;
     }
 
-    public void setGeneratedAt(LocalDateTime generatedAt)
-    {
-        this.generatedAt=generatedAt;
-
+    public void setUser(User user) {
+        this.user = user;
     }
-    public LocalDateTime getGeneratedAt()
-    {
+
+    public LocalDateTime getGeneratedAt() {
         return generatedAt;
     }
 
-    public void setRecommendedLessonIds(String recommendedLessonIds)
-    {
-        this.recommendedLessonIds=recommendedLessonIds;
-
+    public void setGeneratedAt(LocalDateTime generatedAt) {
+        this.generatedAt = generatedAt;
     }
-    public String getRecommendedLessonIds()
-    {
+
+    public String getRecommendedLessonIds() {
         return recommendedLessonIds;
     }
 
-    public void setBasisSnapshot(String basisSnapshot)
-    {
-        this.basisSnapshot=basisSnapshot;
-
+    public void setRecommendedLessonIds(String recommendedLessonIds) {
+        this.recommendedLessonIds = recommendedLessonIds;
     }
-    public String getBasisSnapshot()
-    {
+
+    public String getBasisSnapshot() {
         return basisSnapshot;
     }
 
-    public void setConfidenceScore(Double confidenceScore)
-    {
-        this.confidenceScore=confidenceScore;
+    public void setBasisSnapshot(String basisSnapshot) {
+        this.basisSnapshot = basisSnapshot;
     }
-    public Double getConfidenceScore()
-    {
+
+    public Double getConfidenceScore() {
         return confidenceScore;
     }
+
+    public void setConfidenceScore(Double confidenceScore) {
+        this.confidenceScore = confidenceScore;
+    }
+
+    
+
 }
