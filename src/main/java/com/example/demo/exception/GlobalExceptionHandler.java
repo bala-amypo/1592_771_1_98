64cadf846f078264cccc9e1bf236ce70ee8018a9
bala-amypo <bala@amypo.in>
2025-12-21 18:23:32 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,33 +1,48 @@
 package com.example.demo.exception;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return ResponseEntity.status(400)
+                .body(Map.of("timestamp", LocalDateTime.now(),"errors", errors));
+    }
+
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<?> handleNotFound(ResourceNotFoundException ex,WebRequest req)
-    {
+    public ResponseEntity<?> handleNotFound(ResourceNotFoundException ex, WebRequest req) {
         return ResponseEntity.status(404)
-        .body(Map.of("timestamp", LocalDateTime.now(), "message", ex.getMessage()));
+                .body(Map.of("timestamp", LocalDateTime.now(), "message", ex.getMessage()));
     }
 
     @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<?> handleValidation(ValidationException ex){
+    public ResponseEntity<?> handleValidation(ValidationException ex) {
         return ResponseEntity.status(400)
-        .body(Map.of("timestamp", LocalDateTime.now(), "message", ex.getMessage()));
+                .body(Map.of("timestamp", LocalDateTime.now(), "message", ex.getMessage()));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<?> handleBadRequest(IllegalArgumentException ex)
-    {
+    public ResponseEntity<?> handleBadRequest(IllegalArgumentException ex) {
         return ResponseEntity.status(400)
-        .body(Map.of("timestamp", LocalDateTime.now(), "message", ex.getMessage()));
+                .body(Map.of("timestamp", LocalDateTime.now(), "message", ex.getMessage()));
     }
+
 }
