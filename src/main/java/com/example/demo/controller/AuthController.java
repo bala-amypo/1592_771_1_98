f09b8bb6359f465544baacef80d547a47269376b
bala-amypo <bala@amypo.in>
2025-12-21 15:14:32 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
@@ -31,8 +32,8 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<User> login(@RequestBody User user) {
-        User u = userService.login(user.getEmail(), user.getPassword());
+    public ResponseEntity<User> login(@PathVariable String email,@PathVariable String password) {
+        User u = userService.login(email,password);
         return ResponseEntity.status(201).body(u);
     }
 
