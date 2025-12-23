68923de0d9c1dc5aa76d96803455c04b80243c0b
bala-amypo <bala@amypo.in>
2025-12-23 16:08:30 +0000

    User commit

--- src/main/java/com/example/demo/repository/RecommendationRepository.java
+++ src/main/java/com/example/demo/repository/RecommendationRepository.java
@@ -1,16 +1,32 @@
+// package com.example.demo.repository;
+
+// import java.time.LocalDateTime;
+// import java.util.List;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+
+// import com.example.demo.entity.Recommendation;
+
+// @Repository
+// public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
+//     List<Recommendation> findByUserIdAndGeneratedAtBetween(Long userId, LocalDateTime start, LocalDateTime end);
+
+//     List<Recommendation> findByUserIdOrderByGeneratedAtDesc(Long userId);
+// }
+
+
+
 package com.example.demo.repository;
+
+import com.example.demo.model.Recommendation;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.example.demo.entity.Recommendation;
-
-@Repository
 public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
-    List<Recommendation> findByUserIdAndGeneratedAtBetween(Long userId, LocalDateTime start, LocalDateTime end);
-
     List<Recommendation> findByUserIdOrderByGeneratedAtDesc(Long userId);
+    List<Recommendation> findByUserIdAndGeneratedAtBetween(
+            Long userId, LocalDateTime start, LocalDateTime end);
 }
