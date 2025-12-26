8b2278959197e530463d346c5d5e1f864196e50e
bala-amypo <bala@amypo.in>
2025-12-26 08:58:20 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,9 +1,37 @@
+// package com.example.demo.config;
+
+// import io.swagger.v3.oas.models.OpenAPI;
+// import io.swagger.v3.oas.models.servers.Server;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import java.util.List;
+
+// @Configuration
+// public class SwaggerConfig {
+
+//     @Bean
+//     public OpenAPI customOpenAPI() {
+//         return new OpenAPI()
+//                 // You need to change the port as per your server
+//                 .servers(List.of(
+//                         new Server().url("https://9116.pro604cr.amypo.ai/")
+                        
+//                 ));
+//         }
+// }
+
+
 package com.example.demo.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
@@ -12,10 +40,31 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
+
+                /* ------------------------------
+                 * API Information
+                 * ------------------------------ */
+                .info(new Info()
+                        .title("JWT Demo API")
+                        .version("1.0")
+                        .description("Simple JWT Demo Project for Students"))
+
+                /* ------------------------------
+                 * Server Configuration
+                 * ------------------------------ */
                 .servers(List.of(
                         new Server().url("https://9116.pro604cr.amypo.ai/")
-                        
-                ));
-        }
+                ))
+
+                /* ------------------------------
+                 * Security Configuration
+                 * ------------------------------ */
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Enter JWT token")));
+    }
 }
