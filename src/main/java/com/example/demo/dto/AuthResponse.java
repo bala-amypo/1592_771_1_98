ef6c5cd6b4f7c4f4aaa0302b85ce5338820ea05f
bala-amypo <bala@amypo.in>
2025-12-26 16:18:14 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,5 +1,9 @@
 package com.example.demo.dto;
 
+import lombok.Builder;
+
+@Builder
+
 public class AuthResponse {
 
     private String accessToken;   // IMPORTANT: tests use getAccessToken()
@@ -8,5 +12,54 @@ public class AuthResponse {
     private String role;
     private String message;
 
-    
+    public AuthResponse() {
+    }
+
+    public AuthResponse(String accessToken, Long userId, String email, String role, String message) {
+        this.accessToken = accessToken;
+        this.userId = userId;
+        this.email = email;
+        this.role = role;
+        this.message = message;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
